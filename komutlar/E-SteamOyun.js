b8c883ab675d037e33af539acbfe8b7e03c9ad98
Glitch (flowery-toothsome-accelerator) <none>
2021-03-02 16:22:57 +0000

    🏌📡 Checkpoint ./komutlar/E-SteamOyun.js:23928587/5308

--- komutlar/E-SteamOyun.js
+++ komutlar/E-SteamOyun.js
@@ -2,16 +2,19 @@ const Discord = require("discord.js");
 var steam = require("steam-provider");
 var provider = new steam.SteamProvider();
 
-  let prefix = process.env.prefix;
+let prefix = process.env.prefix;
 
 exports.run = (client, message, args) => {
   let game = args[0];
   let steampng =
     "https://cdn.discordapp.com/attachments/458004691402489856/470344660364034049/steam.png";
   if (!game)
-    return  message.channel.send(new Discord.MessageEmbed() .setColor("BLACK").setDescription(
-      `<@${message.author.id}> | Lütfen Steamde Olan Bir Oyunun Adını Yazın. Örnek: \`${prefix}steam [Oyun]\``
-      )
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setColor("BLACK")
+        .setDescription(
+          `<@${message.author.id}> | Lütfen Steamde Olan Bir Oyunun Adını Yazın. Örnek: \`${prefix}steamoyun [Oyun]\``
+        )
     );
   provider.search(game).then(result => {
     provider.detail(result[0].id, "turkey", "tr").then(results => {
@@ -34,7 +37,15 @@ exports.run = (client, message, args) => {
         .addField("Etiketleri", results.otherData.features, true)
         .addField("Geliştiricileri", results.otherData.developer, true)
         .addField("Yayımcıları", results.otherData.publisher)
-        .setColor("BLACK");
+        .setColor("BLACK")
+        .setFooter(
+          `${message.author.username} tarafından istendi.`,
+          userinfo.avatar
+        );
+      let user = message.mentions.users.first() || message.author;
+
+      let userinfo = {};
+      userinfo.avatar = user.avatarURL();
       message.channel.send(embed).catch(e => {
         console.log(e);
         message.reply("Hata Olustu Yada `" + game + "` Adlı Oyun Bulunamadı");
