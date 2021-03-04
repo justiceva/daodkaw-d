4c295d26fdf2a802551e50d7789f02c44a7bbf2f
Glitch (flowery-toothsome-accelerator) <none>
2021-03-04 20:44:38 +0000

    ☎️⏰ Checkpoint ./komutlar/Öneri.js:23928587/2393

--- komutlar/Öneri.js
+++ komutlar/Öneri.js
@@ -1,39 +1,40 @@
-const Discord = require('discord.js');
-
+const Discord = require("discord.js");
 
 exports.run = function(client, message, args) {
+  var öneri = args.slice(0).join(" ");
+  var guildID = "814905098505420820";
+  var channelID = "814942665027616838";
 
-	var öneri = args.slice(0).join(' ');
-	var guildID = "814905098505420820";
-	var channelID = "814942665027616838";
-	
-	if (!öneri){
-		return message.reply("Bir mesaj belirtin! Doğru kullanım: **byf!öneri <mesaj>**");
-	} else {
-		
-		var embed = new Discord.MessageEmbed()
-			.setTimestamp()
-			.addField("Eylem:", "Öneri")
-			.addField("Kullanıcı:", message.author.tag)
-			.addField("ID", message.author.id)
-			.addField("Öneri", '```'+öneri+'```', true)
-		
-		client.guilds.cache.get(guildID).channels.cache.get(channelID).send(embed);
-		message.channel.send("Öneriniz alınmıştır! Teşekkür ederiz.");
-	};
+  if (!öneri) {
+  if (!öneri)
+    return message.reply(
+      "Bir mesaj belirtin! Doğru kullanım: **byf!öneri <mesaj>**"
+    );
+  } else {
+    var embed = new Discord.MessageEmbed()
+      .setTimestamp()
+      .addField("Eylem:", "Öneri")
+      .addField("Kullanıcı:", message.author.tag)
+      .addField("ID", message.author.id)
+      .addField("Öneri", "```"+ öneri +"```", true);
 
-
+    client.guilds.cache
+      .get(guildID)
+      .channels.cache.get(channelID)
+      .send(embed);
+    message.channel.send("Öneriniz alınmıştır! Teşekkür ederiz.");
+  }
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["öner"], 
-  permLevel: 0 
+  aliases: ["öner"],
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'öneri', 
-  description: "bot hakkındaki önerilerinizi bot sahiplerine ulaştırır", 
-  usage: 'öneri <mesaj>' 
+  name: "öneri",
+  description: "bot hakkındaki önerilerinizi bot sahiplerine ulaştırır",
+  usage: "öneri <mesaj>"
 };
