7d3223cb155cdf2e3e447c6c5c5fd3de75c7c4b3
Glitch (flowery-toothsome-accelerator) <none>
2021-03-04 21:07:19 +0000

    📼🌇 Checkpoint ./komutlar/Öneri.js:23928587/2990

--- komutlar/Öneri.js
+++ komutlar/Öneri.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 
-exports.run = function(client, message, args) {
+exports.run = function(client, message, args) { 
+   var öneri = [
+      'https://wallpaper.dog/large/10816358.png','https://media.discordapp.net/attachments/814947046405570581/817140239859580948/indir.jpg'];
+
+      var öneriler = öneri[Math.floor(Math.random() * öneri.length)];
   var öneri = args.slice(0).join(" ");
   var guildID = "814905098505420820";
   var channelID = "814942665027616838";
@@ -12,8 +16,9 @@ exports.run = function(client, message, args) {
   } else {
     var embed = new Discord.MessageEmbed()
       .setTimestamp()
-      .addField("Kullanıcı:", message.author.tag)
-      .addField(`Öneri`, "```diff + öneri +```", true);
+      .setAuthor(`${message.author.username}`)
+    .setThumbnail(öneriler)
+      .setDescription("```" + öneri + "```");
 
     client.guilds.cache
       .get(guildID)
