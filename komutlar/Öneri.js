138279805f8da5e2ba18ecdab2b0ca7709ae3c16
Glitch (flowery-toothsome-accelerator) <none>
2021-03-04 21:18:43 +0000

    🌋🌽 Checkpoint ./komutlar/Öneri.js:23928587/875

--- komutlar/Öneri.js
+++ komutlar/Öneri.js
@@ -1,30 +1,39 @@
 const Discord = require("discord.js");
 
 exports.run = function(client, message, args) { 
+  let prefix = process.env.prefix;
    var öneri = [
       'https://wallpaper.dog/large/10816358.png','https://media.discordapp.net/attachments/814947046405570581/817140239859580948/indir.jpg'];
 
       var öneriler = öneri[Math.floor(Math.random() * öneri.length)];
   var öneri = args.slice(0).join(" ");
-  var guildID = "814905098505420820";
-  var channelID = "814942665027616838";
+  var guildID = "773159215631958028";
+  var channelID = "813073099704631346";
 
   if (!öneri) {
-    return message.reply(
-      "Bir mesaj belirtin! Doğru kullanım: **byf!öneri <mesaj>**"
+    return  message.channel.send(new Discord.MessageEmbed()
+                         .setColor("BLACK")
+                         .setDescription(
+      `<@${message.author.id}> | Bir mesaj belirtin! Doğru kullanım ➣ ${prefix}öneri <mesaj>`
+      )
     );
   } else {
     var embed = new Discord.MessageEmbed()
-      .setTimestamp()
-      .setAuthor(`${message.author.username}`)
+      .setAuthor(`${message.author.username} | Sunucu İçin Önerisini Yaptı`)
     .setThumbnail(öneriler)
-      .setDescription("```" + öneri + "```");
+    .setColor("BLACK")
+      .setDescription("```" + öneri + "```")
+       .setTimestamp()
+    .setFooter(`Play.ShadowRise.Com`, client.user.avatarURL())
 
     client.guilds.cache
       .get(guildID)
       .channels.cache.get(channelID)
       .send(embed);
-    message.channel.send("Öneriniz alınmıştır! Teşekkür ederiz.");
+    message.channel.send(new Discord.MessageEmbed()
+                         .setColor("BLACK")
+                         .setDescription(`<@${message.author.id}> | Öneriniz alınmıştır! Teşekkür ederiz. ➣ <#813073099704631346> Gelmiştir.`)
+                         );
   }
 };
 
