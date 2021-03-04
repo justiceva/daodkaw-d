0d45881fe20bd06554493d21a1bd17eec1f703e6
Glitch (flowery-toothsome-accelerator) <none>
2021-03-04 12:20:00 +0000

    📅🚛 Checkpoint ./komutlar/Güvenlik.js:23928587/2864

--- komutlar/Güvenlik.js
+++ komutlar/Güvenlik.js
@@ -1,32 +1,47 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
+const Discord = require("discord.js");
+const db = require("quick.db");
 
-exports.run = async(client, message, args) => {
+exports.run = async (client, message, args) => {
+  let prefix = process.env.prefix;
+  if (!message.member.hasPermission("MANAGE_GUILD"))
+    return message.channel.send(
+      new Discord.MessageEmbed() .setColor("BLACK").setDescription(
+      `<@${message.author.id}> | Bu komutu kullanabilmek için \`Sunucuyu yönet\` yetkisine sahip olmalısın`
+    )
+      );
 
-let prefix = process.e.prefix
-   if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('Bu komutu kullanabilmek için `Sunucuyu yönet` yetkisine sahip olmalıısn')
-  
-   let kanal = message.mentions.channels.first() || args[0]
-   if(!kanal) return message.channel.send('Güvenlik mesajlarının gideceği kanalı etiketlemedin :x:')
-   else {
-    db.set(`güvenlik.${message.guild.id}`, kanal.id)
-    return message.channel.send('Güvenlik kanalı <#'+kanal+'> olarak ayarlandı')
-   }
-   if(args[0] === 'sıfırla') {
-    db.delete(`güvenlik.${message.guild.id}`)
-    message.channel.send('Güvenlik kanalı sıfırlandı.')
-   }
-}
+  let kanal = message.mentions.channels.first() || args[0];
+  if (!kanal)
+    return message.channel.send(
+       new Discord.MessageEmbed() .setColor("BLACK").setDescription(
+      `<a:hayir:815534736725901322> | Güvenlik mesajlarının gideceği kanalı etiketlemedin`
+         )
+    );
+  else {
+    db.set(`güvenlik.${message.guild.id}`, kanal.id);
+    return message.channel.send(
+       new Discord.MessageEmbed() .setColor("BLACK").setDescription(
+      "<:rules:816959237439750155> | Güvenlik Kanalı <#" + kanal + "> Olarak Ayarlandı."
+         )
+    );
+  }
+  if (args[0] === "sıfırla") {
+    db.delete(`güvenlik.${message.guild.id}`);
+    message.channel.send(
+      new Discord.MessageEmbed() .setColor("BLACK").setDescription(`<a:evet:815534728493006858> | Başarıyla Güvenlik Kanalı Sıfırlandı.`)
+    );
+  }
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases:[],
+  aliases: [],
   permlevel: 0
 };
 
 exports.help = {
   name: "güvenlik",
-  description: 'Güvenlik kanalını ayarlarsınız.',
-  usage: 'güvenlik #kanal'
-}
+  description: "Güvenlik kanalını ayarlarsınız.",
+  usage: "güvenlik #kanal"
+};
