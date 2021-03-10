11a51188cc0bcef36f176336ae3d3ea21b00cb0f
Glitch (flowery-toothsome-accelerator) <none>
2021-03-10 15:39:14 +0000

    🐀🕰 Checkpoint ./package.json:23928587/3021 ./komutlar/O-Yılan.js:23928587/2236 ./komutlar/Oyunlar.js:23928587/202 ./komutlar/E-Youtube.js:23928587/5993 ./index.html:23928587/7235 ./now-ui-kit.min.js:23928587/4483 ./main.css:23928587/8850 ./now-ui-kit.css:23928587/179721 ./bootstrap.min.css:23928587/155759 ./.glitch-assets:23928587/2857 ./bot.js:23928587/836

---
+++ komutlar/O-Yılan.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const GameCord = require("gamecord-fork").djs;
+const db = require("quick.db");
+
+exports.run = async (client, message, args) => {
+  const embed = new GameCord.SnakeGame(message)
+    .setTitle("Yılan Oyunu")
+    .setColor("BLACK")
+    .setTime(60000) // Always better to set max time because the default one is just 5s
+    .run();
+  message.channel.send(embed);
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["snake", "yılan", "yılan-oyunu", "play-snake", "yılan-oyna"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "snake",
+  description: "Bot i",
+  usage: "istatistik"
+};
