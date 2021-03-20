c820e331213c3e4c80853e6dd3da71cb87f82f72
Glitch (flowery-toothsome-accelerator) <none>
2021-03-20 21:59:51 +0000

    🏗🍐 Checkpoint ./events/ready.js:29941633/1626

--- events/ready.js
+++ events/ready.js
@@ -1,25 +1,28 @@
-const chalk = require("chalk");
-const moment = require("moment");
-const Discord = require("discord.js");
-
-let prefix = process.env.prefix;
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
 
 module.exports = client => {
-  console.log(
-    ` [${moment().format(
-      "YYYY-MM-DD HH:mm:ss"
-    )}] BOT: Aktif, Komutlar yüklendi!`
-  );
-  console.log(
-    ` [${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
-      client.user.username
-    } ismi ile giriş yapıldı!`
-  );
-  client.user.setStatus("idle");
 
-  client.user.setActivity(`ShadowRise Network`, { type: "WATCHING" });
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("dnd");
+   var oyun = [
 
-  console.log(
-    ` [${moment().format("YYYY-MM-DD HH:mm:ss")}] Oyun ismi ayarlandı!`
-  );
-};
+        "ShadowRise Network Gelişmiş Discord Bot",
+        "Herşeyi Ayarlamalı",
+        "Minecraft Sunucusu Yakında Açılıyor",
+         "Öneri Yapmak İçin sn!öneri komutuyla Öneri Yapabilirsiniz",
+     
+     
+
+       
+ 
+   ];
+  setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "idle"); 
+        }, 2 * 3000);
+}
