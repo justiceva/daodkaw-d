a72af4dfb7e86c52dee03dbff455fe7875a08f0e
Glitch (flowery-toothsome-accelerator) <none>
2021-03-22 17:22:53 +0000

    🏔🏜 Checkpoint ./komutlar/G-Say.js:29941633/6161

--- komutlar/G-Say.js
+++ komutlar/G-Say.js
@@ -1,32 +1,62 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    if (!message.guild) return message.author.send('Bu Komutu Sadece Sunucularda Kulanabilirsiniz!');
+  if (!message.guild)
+    return message.author.send(
+      "Bu Komutu Sadece Sunucularda Kulanabilirsiniz!"
+    );
 
- 
-    const say = new Discord.MessageEmbed()
-        .setColor(message.guild.me.displayColor)
+  const say = new Discord.MessageEmbed()
+    .setColor(message.guild.me.displayColor)
     .setTitle(message.guild.name)
-        .addField("🌐 Sunucudaki üye sayısı", `message.guild.memberCount`)
-        .addField("🟢 Çevrimiçi üye sayısı", message.guild.members.cache.filter(m => m.user.presence.status !== "offline").size)
-        .addField("🔘 Çevrimdışı üye sayısı", message.guild.members.cache.filter(m => m.user.presence.status == "offline").size)
-        .addField("🤖 Sunucudaki Bot Sayısı", message.guild.members.cache.filter(m => m.user.bot).size)
-        .addField(`🍭 **__Boost Sayısı__**`,`**${message.guild.premiumSubscriptionCount}**`)
-    .addField(`Sunucudaki Emoji Sayısı`, `${message.guild.emojis.cache.size}`)
-    .addField(`Sunucudaki Rol Sayısı`, `${message.guild.roles.cache.size}`)
- 
-    message.channel.send(say);
+    .addField(
+      "<a:guard:822552681863315516> **__Sunucudaki Toplam Üye Sayısı__**",
+      `\`\`\`${message.guild.memberCount}\`\`\``
+    )
+    .addField(
+      `**__Sunucudaki Toplam Kanal Sayısı__**`,
+      `  \`\`\`${message.guild.channels.cache.size}\`\`\``
+    )
+    .addField(
+      "🟢 **__Çevrimiçi üye sayısı__**",
+      message.guild.members.cache.filter(
+        m => m.user.presence.status !== "offline"
+      ).size
+    )
+    .addField(
+      "🔘 **__Çevrimdışı üye sayısı__**",
+      message.guild.members.cache.filter(
+        m => m.user.presence.status == "offline"
+      ).size
+    )
+    .addField(
+      "🤖 **__Sunucudaki Bot Sayısı__**","»"
+      message.guild.members.cache.filter(m => m.user.bot).size
+    )
+    .addField(
+      `<a:boost:823605267769065542> **__Boost Sayısı__**`,
+      `» **${message.guild.premiumSubscriptionCount}**`
+    )
+    .addField(
+      `<a:emoji:823606451922665543> **__Sunucudaki Emoji Sayısı__**`,
+      `» ${message.guild.emojis.cache.size}`
+    )
+    .addField(
+      `<a:rol:823605264854548481> **__Sunucudaki Rol Sayısı__**`,
+      `» ${message.guild.roles.cache.size}`
+    );
 
-}
+  message.channel.send(say);
+};
 
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['say'],
-    permLevel: 0
+  enabled: true,
+  guildOnly: false,
+  aliases: ["say"],
+  permLevel: 0
 };
 
 exports.help = {
-    name: 'gelişmiş-say',
-    description: 'Say',
- }
+  name: "gelişmiş-say",
+  description: "Say"
+};
