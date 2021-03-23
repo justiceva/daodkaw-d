d8df643a8e4cf322ef89aca9b69f259d42a29f5a
Glitch (shadowrise) <none>
2021-03-23 12:54:51 +0000

    🏤💽 Checkpoint ./komutlar/Seviye-Sistem.js:29941633/4715

---
+++ komutlar/Seviye-Sistem.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const prefix = process.env.prefix;
+exports.run = async (bot, msg, args) => {
+  const seviye = new Discord.MessageEmbed()
+    .setAuthor(`ShadowRise Network | Seviye Sistem`)
+    .setTitle(``)
+    .setColor("#00ff00")
+    .setThumbnail(
+      "https://media.discordapp.net/attachments/822526781839245332/822526805734850600/20210319_160634.png"
+    )
+    .setDescription(
+      `<a:tik:822531200353959937> ShadowRise Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
+    )
+    .addField(
+      `**__Seviye__**`,
+      `<a:green:822530401301954632> \`${prefix}seviye\` \n Sizin Özelliştirdiginiz Seviye Sıralama Kartını Gösterir.`,
+        true
+    )
+     .addField(
+      `**__Seviye Sıralama__**`,
+      `<a:green:822530401301954632> \`${prefix}sıralama\` \n Seviye DataBase 'indeki Sıralama Ranklarını Görürsün.`,
+        true
+    )
+    .addField(
+      `**__Seviye Aç__**`,
+      `<a:green:822530401301954632> \`${prefix}seviye-ayarla durum aç\` \n Discord Sunucunuzda Seviye Sistemini Açarsınız.`,
+        true
+    )
+     .addField(
+      `**__Seviye Kapat__**`,
+      `<a:green:822530401301954632> \`${prefix}seviye-ayarla durum kapat\` \n Discord Sunucunuzda Seviye Sistemini Kapatırsınız.`,
+        true
+    )
+     .addField(
+      `**__Seviye Kanal__**`,
+      `<a:green:822530401301954632> \`${prefix}seviye-ayarla log kanal\` \n Discord Sunucunuzda Seviye Log Kanal Ayarlarsanız.`,
+        true
+    )
+     .addField(
+      `**__Seviye Mesaj__**`,
+      `<a:green:822530401301954632> \`${prefix}seviye-ayarla log mesaj seviye\` \n Seviye Log Kanalına Gidecek Mesaji Ayarlasınız.`,
+        true
+    )
+    .addField(
+      `__Bilgilendirme__`,
+      `<a:yesil:822529538663514173>  \`${prefix}davet\` | Botu Sununuya Davet Edersiniz\n <a:yesil:822529538663514173> \`${prefix}botbilgi\` | Botun İstatistiklerini Görürsünüz \n <a:yesil:822529538663514173> \`${prefix}iletişim\` | ShadowRise İletişim Bilgileri.`
+    );
+  msg.channel.send(seviye);
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["", ""],
+  permLevel: 0
+};
+exports.help = {
+  name:"seviye-sistem",
+  description: "İstediğiniz kullanıcını bilgilerini gösterir.",
+  usage: "seviye"
+};
