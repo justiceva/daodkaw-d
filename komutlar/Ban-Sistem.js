0e7f7ded382c934cef8b39b33373c401cd482a4b
Glitch (shadowrise) <none>
2021-03-24 14:25:34 +0000

    🍟🎸 Checkpoint ./komutlar/Öneri-Sistem.js:29941633/6 ./komutlar/Seviye-Sistem.js:29941633/6 ./komutlar/Profil-Sistem.js:29941633/6 ./komutlar/OtoRol-Sistem.js:29941633/6 ./komutlar/Ban-Sistem.js:29941633/6

--- komutlar/Ban-Sistem.js
+++ komutlar/Ban-Sistem.js
@@ -35,7 +35,7 @@ exports.run = async (bot, msg, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["", ""],
+  aliases: [],
   permLevel: 0
 };
 exports.help = {
--- komutlar/Profil-Sistem.js
+++ komutlar/Profil-Sistem.js
@@ -50,7 +50,7 @@ exports.run = async (bot, msg, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ["", ""],
+  aliases: [],
   permLevel: 0
 };
 exports.help = {
