54e4f2ce25cd5f89806bffbc049b12579b24b6db
Glitch (shadowrise) <none>
2023-05-19 10:35:39 +0000

    🏅🐮 Checkpoint ./README.md:14411661/28 ./komutlar/Bakım.js:14411661/87

--- komutlar/Bakım.js
+++ komutlar/Bakım.js
@@ -11,15 +11,15 @@ let CodEming = args.slice(0).join(' ') || ' Belirtilmemiş.'
 
 if(message.author.id !== sahipID) return message.channel.send(' Sadece **sahibim** bakım modu ayarlayabilir.')
 
-let meteyasin = CodEmingHerDaim.fetch("!methesu7washere")
+let meteyasin = CodEmingHerDaim.fetch("8182bakımaç81")
 
 if(meteyasin) {
-CodEmingHerDaim.delete("!methesu7washere")
+CodEmingHerDaim.delete("8182bakımaç81")
 return message.channel.send(' Bakım sistemi kapatıldı! ')
 }
 
 if(!meteyasin) {
-CodEmingHerDaim.set("!methesu7washere", CodEming)
+CodEmingHerDaim.set("8182bakımaç81", CodEming)
 return message.channel.send(" Tamamdır! Bakım sistemi aktif edildi! Bakım sebebi: **"+CodEming+"**")
 }
 }
